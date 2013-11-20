206bb8dd68684c78959f44165f7b411d9c41c9ff
Dawid Pogorzelski <shogun999@gmail.com>
2013-11-20 11:17:11 +0100

    sing in and user controller added. user profile created

--- js/controllers.js
+++ js/controllers.js
@@ -3,8 +3,19 @@ app.controller('homeCtrl', ['$scope', '$http',
   function ($scope, $http) {}
 ]);
 
-app.controller('loginCtrl', ['$scope', '$http',
-  function ($scope, $http) {
+app.controller('signinCtrl', ['$scope', '$http', '$location', '$cookies',
+  function ($scope, $http, $location, $cookies) {
+    $scope.signin = function () {
+      $http.get('/data/user.json').success(function (user) {
+        $cookies.user = JSON.stringify(user);
+        $location.path('/user');
+      });
+    };
+  }
+]);
 
+app.controller('userCtrl', ['$scope', '$http', '$cookies',
+  function ($scope, $http, $cookies) {
+    $scope.user = JSON.parse($cookies.user);
   }
 ]);
