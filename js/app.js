cef7b9845603084937531eb15c218942c17c7fe3
Dawid Pogorzelski <shogun999@gmail.com>
2013-12-05 15:43:08 +0100

    service and organization routes added

--- js/app.js
+++ js/app.js
@@ -25,8 +25,34 @@ app.config(['$routeProvider', '$locationProvider',
       controller: 'profileCtrl',
       templateUrl: '/partials/profile/show.html'
     }).
+    when('/profile/services/new', {
+      controller: 'newServiceCtrl',
+      templateUrl: '/partials/profile/services/edit.html'
+    }).
+    when('/profile/services/:id/edit', {
+      controller: 'editServiceCtrl',
+      templateUrl: '/partials/profile/services/edit.html'
+    }).
+    when('/profile/organizations/new', {
+      controller: 'newOrgCtrl',
+      templateUrl: '/partials/profile/organizations/edit.html'
+    }).
+    when('/profile/organizations/:id/edit', {
+      controller: 'editOrgCtrl',
+      templateUrl: '/partials/profile/organizations/edit.html'
+    }).
     otherwise({
       redirectTo: '/'
     });
   }
-]);
+]).run(function($rootScope, $location){
+    var history = [];
+
+    $rootScope.$on('$routeChangeSuccess', function() {
+        history.push($location.$$path);
+    });
+    $rootScope.back = function () {
+        var prevUrl = history.length > 1 ? history.splice(-2)[0] : "/";
+        $location.path(prevUrl);
+    };
+})
