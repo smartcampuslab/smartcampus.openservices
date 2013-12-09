009a011df6a7c67e23e8980c698ef86b6f4318c2
Dawid Pogorzelski <shogun999@gmail.com>
2013-12-09 14:17:47 +0100

    prettify added

--- js/directives.js
+++ js/directives.js
@@ -12,3 +12,13 @@ directives.directive('holder', [
     };
   }
 ]);
+
+directives.directive('pp', [
+  function () {
+    return {
+      link: function (scope, element) {
+        prettyPrint();
+      }
+    };
+  }
+]);
