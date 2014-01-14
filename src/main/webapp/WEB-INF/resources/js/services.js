720349a393d1b896871dee23b8ab00ad01b9f1bf
raman <raman@fbk.eu>
2014-01-14 16:27:28 +0100

    corrected merge

--- src/main/webapp/WEB-INF/resources/js/services.js
+++ src/main/webapp/WEB-INF/resources/js/services.js
@@ -41,17 +41,10 @@ services.factory('Auth', ['$http', '$cookieStore', '$rootScope',
             'Content-Type': 'application/x-www-form-urlencoded'
           }
         }).success(function (data) {
-<<<<<<< HEAD
         	console.log('user data: ', data)
           var role = data.role
           var templ = {
         	  username : data.username,
-=======
-                console.log('user data: ', data)
-          var role = data.role
-          var templ = {
-                  username : data.username,
->>>>>>> 5c661b8919f517977613bcced509796b2db63879
             role: userRoles[role]
           };
           
@@ -610,8 +603,4 @@ services.factory('oAuth', ['$http', '$window', '$location', '$rootScope',
       }
     }
   }
-<<<<<<< HEAD
 ]);
-=======
-]);
->>>>>>> 5c661b8919f517977613bcced509796b2db63879
