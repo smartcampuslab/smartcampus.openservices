a10a557f2e507135f389ca0e05b185303df524f2
gcanobbio <g.canobbio@gmail.com>
2014-05-23 10:17:53 +0200

    Social controller: add fb,twitter and linkedin

--- src/main/java/eu/trentorise/smartcampus/openservices/controllers/SocialController.java
+++ src/main/java/eu/trentorise/smartcampus/openservices/controllers/SocialController.java
@@ -1,33 +1,77 @@
+/*******************************************************************************
+ * Copyright 2012-2013 Trento RISE
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package eu.trentorise.smartcampus.openservices.controllers;
-
-import java.net.URLEncoder;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.slf4j.*;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.util.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import eu.trentorise.smartcampus.openservices.entities.ResponseObject;
-
+/**
+ * Social controller for
+ * Facebook
+ * Google Plus
+ * Twitter
+ * Linkedin
+ * @author Giulia Canobbio
+ *
+ */
 @Controller
 @RequestMapping(value = "/api/social")
 public class SocialController {
 
 	private static final Logger logger = LoggerFactory.getLogger(SocialController.class);
 	private RestTemplate temp;
+	
+	
+	@RequestMapping(value = "/fb", method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	public ResponseObject socialFb() {
+		ResponseObject responseObject = new ResponseObject();
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		
+		MultiValueMap<String, String> body = new LinkedMultiValueMap<String, String>();
+		body.add("scope", "email,user_likes,friends_likes,publish_stream");
+		
+		HttpEntity<?> httpEntity = new HttpEntity<Object>(body,headers);
+		
+		ResponseEntity<Object> r =temp.exchange("http://localhost:8080/openservice/signin/facebook", HttpMethod.POST, httpEntity, Object.class);
+		
+		logger.info("## ResponseEntity Headers: {}  ##",r.getHeaders().getLocation());
+		logger.info("## ResponseEntity Body: {} ##",r.getBody());
+		logger.info("## Status code: {} ", r.getStatusCode());
+		
+		if(r.getStatusCode()==HttpStatus.OK){
+			responseObject.setData(r.getHeaders().getLocation());
+			responseObject.setStatus(HttpServletResponse.SC_OK);
+		}
+		else{
+			responseObject.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			responseObject.setError("Facebook unavailable. Retry Later.");
+		}
+		
+		return responseObject;
+	}
 	
 	@RequestMapping(value = "/google", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
@@ -46,9 +90,9 @@ public class SocialController {
 		
 		ResponseEntity<Object> r =temp.exchange("http://localhost:8080/openservice/signin/google", HttpMethod.POST, httpEntity, Object.class);
 		
-		logger.info("## ResponseEntity Headers: " + r.getHeaders().getLocation() + " ##");
-		logger.info("## ResponseEntity Body: " + r.getBody() + " ##");
-		logger.info("## Status code: " + r.getStatusCode());
+		logger.info("## ResponseEntity Headers: {}  ##",r.getHeaders().getLocation());
+		logger.info("## ResponseEntity Body: {} ##",r.getBody());
+		logger.info("## Status code: {} ", r.getStatusCode());
 		
 		if(r.getHeaders().getLocation()!=null){
 			responseObject.setData(r.getHeaders().getLocation());
@@ -57,6 +101,68 @@ public class SocialController {
 		else{
 			responseObject.setStatus(HttpServletResponse.SC_NOT_FOUND);
 			responseObject.setError("Problem with Google Plus. Retry Later.");
+		}
+		
+		return responseObject;
+	}
+	
+	@RequestMapping(value = "/twitter", method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	public ResponseObject socialTwitter() {
+		ResponseObject responseObject = new ResponseObject();
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		
+		MultiValueMap<String, String> body = new LinkedMultiValueMap<String, String>();
+		//body.add("scope", "email,user_likes,friends_likes,publish_stream");
+		
+		HttpEntity<?> httpEntity = new HttpEntity<Object>(body,headers);
+		
+		ResponseEntity<Object> r =temp.exchange("http://localhost:8080/openservice/signin/twitter", HttpMethod.POST, httpEntity, Object.class);
+		
+		logger.info("## ResponseEntity Headers: {}  ##",r.getHeaders().getLocation());
+		logger.info("## ResponseEntity Body: {} ##",r.getBody());
+		logger.info("## Status code: {} ", r.getStatusCode());
+		
+		if(r.getStatusCode()==HttpStatus.OK){
+			responseObject.setData(r.getHeaders().getLocation());
+			responseObject.setStatus(HttpServletResponse.SC_OK);
+		}
+		else{
+			responseObject.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			responseObject.setError("Twitter unavailable. Retry Later.");
+		}
+		
+		return responseObject;
+	}
+	
+	@RequestMapping(value = "/linkedin", method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	public ResponseObject socialLinkedin() {
+		ResponseObject responseObject = new ResponseObject();
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		
+		MultiValueMap<String, String> body = new LinkedMultiValueMap<String, String>();
+		body.add("scope", "r_basicprofile r_emailaddress");
+		
+		HttpEntity<?> httpEntity = new HttpEntity<Object>(body,headers);
+		
+		ResponseEntity<Object> r =temp.exchange("http://localhost:8080/openservice/signin/linkedin", HttpMethod.POST, httpEntity, Object.class);
+		
+		logger.info("## ResponseEntity Headers: {}  ##",r.getHeaders().getLocation());
+		logger.info("## ResponseEntity Body: {} ##",r.getBody());
+		logger.info("## Status code: {} ", r.getStatusCode());
+		
+		if(r.getStatusCode()==HttpStatus.OK){
+			responseObject.setData(r.getHeaders().getLocation());
+			responseObject.setStatus(HttpServletResponse.SC_OK);
+		}
+		else{
+			responseObject.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			responseObject.setError("Linkedin unavailable. Retry Later.");
 		}
 		
 		return responseObject;
