8e4c9f75eb99357b31307be97063c559b01dd15f
gcanobbio <g.canobbio@gmail.com>
2014-07-10 14:35:51 +0200

    Delete not used libraries

--- src/main/java/eu/trentorise/smartcampus/openservices/controllers/HomeController.java
+++ src/main/java/eu/trentorise/smartcampus/openservices/controllers/HomeController.java
@@ -15,8 +15,6 @@
  ******************************************************************************/
 package eu.trentorise.smartcampus.openservices.controllers;
 
-import java.io.IOException;
-
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
