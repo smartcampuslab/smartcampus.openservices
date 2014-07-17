9ed37a28ecda0a80c64c64568d5c088f6dc76b90
gcanobbio <g.canobbio@gmail.com>
2014-07-17 13:28:07 +0200

    Delete not used libraries.

--- src/main/java/eu/trentorise/smartcampus/openservices/controllers/HomeController.java
+++ src/main/java/eu/trentorise/smartcampus/openservices/controllers/HomeController.java
@@ -23,7 +23,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
