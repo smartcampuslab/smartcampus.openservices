14fff37c7c20b679f446ec224d7f59e44358f4df
mirkoperillo <mirko.perillo@gmail.com>
2015-01-27 12:12:21 +0100

    fix #7

--- src/main/webapp/WEB-INF/resources/js/directives.js
+++ src/main/webapp/WEB-INF/resources/js/directives.js
@@ -142,3 +142,29 @@ directives.directive('checkStrength', [
 			}, template: '<li class="point"></li><li class="point"></li><li class="point"></li><li class="point"></li><li class="point"></li>'
 		};	
 }]);
+
+/**
+* A generic confirmation for risky actions.
+* Usage: Add attributes:
+* * ng-confirm-message="Are you sure?"
+* * ng-confirm-click="takeAction()" function
+* * ng-confirm-condition="mustBeEvaluatedToTrueForTheConfirmBoxBeShown" expression
+*/
+directives.directive('ngConfirmClick', [ function() {
+	return {
+		restrict : 'A',
+		link : function(scope, element, attrs) {
+			element.bind('click', function() {
+				var condition = scope.$eval(attrs.ngConfirmCondition);
+				if (condition) {
+					var message = attrs.ngConfirmMessage;
+					if (message && confirm(message)) {
+						scope.$apply(attrs.ngConfirmClick);
+					}
+				} else {
+					scope.$apply(attrs.ngConfirmClick);
+				}
+			});
+		}
+	};
+} ]);
