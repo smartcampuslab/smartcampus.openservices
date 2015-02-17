b0dcb329793e25c9223ca85da87438aebae210a1
raman <raman@fbk.eu>
2015-02-17 18:03:42 +0100

    Gravatar cleanup

--- src/main/webapp/WEB-INF/resources/js/directives.js
+++ src/main/webapp/WEB-INF/resources/js/directives.js
@@ -1,24 +1,6 @@
 'use strict';
 var directives = angular.module('openservices.directives', []);
 
-directives.directive('gravatar', ['Gravatar', '$compile',
-    function (Gravatar, $compile) {
-        return {
-
-            link: function (scope, element, attrs) {
-                attrs.$observe('gravatar', function (email) {
-                    if (email) {
-                        var html = '<img class="img-responsive img-circle" style="width: ' + attrs.size + 'px;height:' + attrs.size + 'px;" src="' + Gravatar.picture(attrs.size, email) + '" />';
-                        var e = $compile(html)(scope);
-                        element.replaceWith(e);
-                    }
-
-                });
-            }
-        };
-    }
-]);
-
 directives.directive('fileselect', ['$parse',
     function ($parse) {
         return function (scope, elem, attr) {
